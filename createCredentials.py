6ce3069a39d2a05c168986f2986fffb42f7574cf
Jyothis <jyothis@hpe.com>
2019-12-16 09:17:53 -0500

    Pep8 fixes.

--- createCredentials.py
+++ createCredentials.py
@@ -7,7 +7,7 @@ Created on Thu Jan 17 08:19:38 2019
 
 from cryptography.fernet import Fernet
 import getpass
-from lxml import etree 
+from lxml import etree
 
 
 uname = input("Username: ")
@@ -20,11 +20,11 @@ port = input("Port: ")
 ovc = input("OVC IP Addr: ")
 fname = input("Filename: ")
 
-keyfile=fname+'.key'
-xmlfile=fname+'.xml'
+keyfile = fname + '.key'
+xmlfile = fname + '.xml'
 key = Fernet.generate_key()
 k1 = key.decode('ASCII')
-f = open(keyfile,'w')
+f = open(keyfile, 'w')
 f.write(key.decode('ASCII'))
 f.close()
 
@@ -33,36 +33,36 @@ token = f.encrypt(password.encode('ASCII'))
 user = f.encrypt(uname.encode('ASCII'))
 
 root = etree.Element("data")
-etree.SubElement(root,"username").text=uname
-etree.SubElement(root,"user").text=user
-etree.SubElement(root,"password").text=token
-etree.SubElement(root,"ovc").text=ovc
-etree.SubElement(root,"timerange").text=timerange
-etree.SubElement(root,"resolution").text=resolution
-etree.SubElement(root,"monitoringintervall").text=monitoring
-etree.SubElement(root,"logfile").text=logfile
-etree.SubElement(root,"port").text=port
+etree.SubElement(root, "username").text = uname
+etree.SubElement(root, "user").text = user
+etree.SubElement(root, "password").text = token
+etree.SubElement(root, "ovc").text = ovc
+etree.SubElement(root, "timerange").text = timerange
+etree.SubElement(root, "resolution").text = resolution
+etree.SubElement(root, "monitoringintervall").text = monitoring
+etree.SubElement(root, "logfile").text = logfile
+etree.SubElement(root, "port").text = port
 
 xmloutput = etree.tostring(root, pretty_print=True)
-f = open(xmlfile,'w')
+f = open(xmlfile, 'w')
 f.write(xmloutput.decode('ASCII'))
 f.close()
 
-""" Test the keys """ 
+""" Test the keys """
 """ Read keyfile """
 f = open(keyfile, 'r')
-k2=f.readline()
+k2 = f.readline()
 f.close()
-key2=k2.encode('ASCII')
+key2 = k2.encode('ASCII')
 
 """ Parse XML File """
 
 tree = etree.parse(xmlfile)
-u2=(tree.find("user")).text
-p2=(tree.find("password")).text
+u2 = (tree.find("user")).text
+p2 = (tree.find("password")).text
 
 
 f = Fernet(key2)
 user = f.decrypt(u2.encode('ASCII')).decode('ASCII')
 password = f.decrypt(p2.encode('ASCII')).decode('ASCII')
-print(user,password)
+print(user, password)
--- svtPromConnector.py
+++ svtPromConnector.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on August 31, 2019
-Version 2.0 
+Version 1.0
 
 Copyright (c) 2019 Hewlett Packard Enterprise
 
@@ -14,24 +14,24 @@ Copyright (c) 2019 Hewlett Packard Enterprise
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-    https://www.gnu.org/licenses/gpl-3.0.en.html 
+    https://www.gnu.org/licenses/gpl-3.0.en.html
 
 """
 
 from cryptography.fernet import *
-from lxml import etree 
+from lxml import etree
 import time
 from SimpliVityClass import *
 from datetime import datetime
 from prometheus_client import Counter, Gauge, start_http_server
 
-BtoGB=pow(1024,3)
-BtoMB=pow(1024,2)
+BtoGB = pow(1024, 3)
+BtoMB = pow(1024, 2)
 path = './'
-#path = '/opt/svt/'
- 
+# path = '/opt/svt/'
 
-node_state={
+
+node_state = {
     'UNKOWN': 0,
     'ALIVE': 1,
     'SUSPECTED': 2,
@@ -40,13 +40,13 @@ node_state={
     'REMOVED': 5
 }
 
-vm_state={
+vm_state = {
     'ALIVE': 1,
     'DELETED': 2,
     'REMOVED': 3
 }
 
-capacitymetric=[
+capacitymetric = [
     'allocated_capacity',
     'free_space',
     'capacity_savings',
@@ -59,13 +59,13 @@ capacitymetric=[
     'stored_virtual_machine_data'
 ]
 
-dedupmetric=[
+dedupmetric = [
     'compression_ratio',
     'deduplication_ratio',
     'efficiency_ratio'
 ]
 
-performancemetric=[
+performancemetric = [
     'read_iops',
     'write_iops',
     'read_throughput',
@@ -74,22 +74,26 @@ performancemetric=[
     'write_latency'
 ]
 
+
 def logwriter(f, text):
-        output=str(datetime.today()) +": "+text+" \n"
+        output = str(datetime.today()) + ": "+text+" \n"
         print(output)
         f.write(output)
-    
+
+
 def logopen(filename):
-        f = open(filename,'a')
+        f = open(filename, 'a')
         f.write(str(datetime.today())+": Logfile opened \n")
         return f
+
 
 def logclose(f):
         f.write(str(datetime.today())+": Logfile closed \n")
         f.close()
 
+
 def getPerformanceAverage(data):
-        perf={
+        perf = {
             'read_iops': 0,
             'write_iops': 0,
             'read_throughput': 0,
@@ -99,19 +103,19 @@ def getPerformanceAverage(data):
         }
         for x in data:
             if x['name'] == 'iops':
-                i=0
+                i = 0
                 for y in x['data_points']:
                     perf['read_iops'] += y['reads']
                     perf['write_iops'] += y['writes']
                     i += 1
-                if i > 0:   
-                    perf['read_iops'] /= i 
+                if i > 0:
+                    perf['read_iops'] /= i
                     perf['write_iops'] /= i
                 else:
-                    perf['read_iops'] = -1 
-                    perf['write_iops'] = -1                    
+                    perf['read_iops'] = -1
+                    perf['write_iops'] = -1
             if x['name'] == 'throughput':
-                i=0
+                i = 0
                 for y in x['data_points']:
                     perf['read_throughput'] += y['reads']
                     perf['write_throughput'] += y['writes']
@@ -121,23 +125,24 @@ def getPerformanceAverage(data):
                     perf['write_throughput'] /= (i * BtoMB)
                 else:
                     perf['read_throughput'] = -1
-                    perf['write_throghput'] = -1 
+                    perf['write_throghput'] = -1
             if x['name'] == 'latency':
-                i=0
+                i = 0
                 for y in x['data_points']:
                     perf['read_latency'] += y['reads']
                     perf['write_latency'] += y['writes']
                     i += 1
                 if i > 0:
-                    perf['read_latency'] /= (i * 1000) 
+                    perf['read_latency'] /= (i * 1000)
                     perf['write_latency'] /= (i * 1000)
                 else:
-                    perf['read_latency'] = -1 
-                    perf['write_latency'] = -1                
+                    perf['read_latency'] = -1
+                    perf['write_latency'] = -1
         return(perf)
 
+
 def getNodeCapacity(data):
-        ndata={
+        ndata = {
             'allocated_capacity': 0,
             'free_space': 0,
             'capacity_savings': 0,
@@ -150,63 +155,63 @@ def getNodeCapacity(data):
             'stored_virtual_machine_data': 0,
             'compression_ratio': 0,
             'deduplication_ratio': 0,
-            'efficiency_ratio': 0                              
+            'efficiency_ratio': 0
         }
         for y in data:
             if 'ratio' in y['name']:
-                ndata[y['name']]=y['data_points'][-1]['value']
-            else: 
+                ndata[y['name']] = y['data_points'][-1]['value']
+            else:
                 ndata[y['name']] = y['data_points'][-1]['value']/BtoGB
         return ndata
 
-### Main ###########################################################################
 
+# ******** Main ********
 if __name__ == "__main__":
-    
-    t0=time.time()
+
+    t0 = time.time()
     """ read the key and input file """
-    keyfile= path + 'SvtConnector.key'
-    xmlfile=path + 'SvtConnector.xml'
+    keyfile = path + 'SvtConnector.key'
+    xmlfile = path + 'SvtConnector.xml'
 
     """ Parse XML File """
     tree = etree.parse(xmlfile)
-    u2=(tree.find("user")).text
-    p2=(tree.find("password")).text
-    ovc=(tree.find("ovc")).text
-    mintervall=int((tree.find("monitoringintervall")).text)
-    mresolution=(tree.find("resolution")).text
-    mrange=(tree.find("timerange")).text
-    lfile=(tree.find("logfile")).text
-    port=int((tree.find("port")).text)
+    u2 = (tree.find("user")).text
+    p2 = (tree.find("password")).text
+    ovc = (tree.find("ovc")).text
+    mintervall = int((tree.find("monitoringintervall")).text)
+    mresolution = (tree.find("resolution")).text
+    mrange = (tree.find("timerange")).text
+    lfile = (tree.find("logfile")).text
+    port = int((tree.find("port")).text)
 
-    """ Open the logfile """ 
-    log=logopen(path+lfile)
+    """ Open the logfile """
+    log = logopen(path+lfile)
 
     """ Read keyfile """
     f = open(keyfile, 'r')
-    k2=f.readline()
+    k2 = f.readline()
     f.close()
-    key2=k2.encode('ASCII')
+    key2 = k2.encode('ASCII')
     f = Fernet(key2)
 
     """ Create the SimpliVity Rest API Object"""
-    logwriter(log,"Open a connection to the SimpliVity systems")
+    logwriter(log, "Open a connection to the SimpliVity systems")
     svtuser = f.decrypt(u2.encode('ASCII')).decode('ASCII')
     svtpassword = f.decrypt(p2.encode('ASCII')).decode('ASCII')
-    url="https://"+ovc+"/api/"         
+    url = "https://"+ovc+"/api/"
     svt = SimpliVity(url)
-    logwriter(log,"Open Connection to SimpliVity")
-    svt.Connect(svtuser,svtpassword)
-    logwriter(log,"Connection to SimpliVity is open")
+    logwriter(log, "Open Connection to SimpliVity")
+    svt.Connect(svtuser, svtpassword)
+    logwriter(log, "Connection to SimpliVity is open")
     logclose(log)
-        
+
     start_http_server(port)
-    c = Counter('simplivity_sample','SimpliVity sample number')
-    scluster = Gauge('simplivity_cluster','SimpliVity Cluster Data',['clustername','clustermetric'])
-    snode = Gauge('simplivity_node','SimpliVity Node Data',['nodename','nodemetric'])
-    svm = Gauge('simplivity_vm','SimpliVity VM Data',['vmname','vmmetric'])
-    sdatastore = Gauge('simplivity_datastore','SimpliVity Datastore Data - Sizes in GB',['dsname','dsmetric'])
-    delta = Gauge('ConnectorRuntime','Time required for last data collection in seconds')
+    c = Counter('simplivity_sample', 'SimpliVity sample number')
+    scluster = Gauge('simplivity_cluster', 'SimpliVity Cluster Data', ['clustername', 'clustermetric'])
+    snode = Gauge('simplivity_node', 'SimpliVity Node Data', ['nodename', 'nodemetric'])
+    svm = Gauge('simplivity_vm', 'SimpliVity VM Data', ['vmname', 'vmmetric'])
+    sdatastore = Gauge('simplivity_datastore', 'SimpliVity Datastore Data - Sizes in GB', ['dsname', 'dsmetric'])
+    delta = Gauge('ConnectorRuntime', 'Time required for last data collection in seconds')
 
     """
     Start an endless loop to capture the current status every TIME_RANGE
@@ -214,101 +219,104 @@ if __name__ == "__main__":
     Please note that the connection must be refreshed after 24h or afte 10 minutes inactivity.
     """
 
-    while True:                
+    while True:
         try:
-            t0 = time.time()         
+            t0 = time.time()
             c.inc()
             clusters = svt.GetCluster()['omnistack_clusters']
             hosts = svt.GetHost()['hosts']
             vms = svt.GetVM()['virtual_machines']
             datastores = svt.GetDataStore()['datastores']
-            scluster.labels('Federation','Cluster_count').set(len(clusters))
-            snode.labels('Federation','Node_count').set(len(hosts))
-            svm.labels('Federation','VM_count').set(len(vms))
-            sdatastore.labels('Federation','Datastore_count').set(len(datastores))
+            scluster.labels('Federation', 'Cluster_count').set(len(clusters))
+            snode.labels('Federation', 'Node_count').set(len(hosts))
+            svm.labels('Federation', 'VM_count').set(len(vms))
+            sdatastore.labels('Federation', 'Datastore_count').set(len(datastores))
             """  Cluster metrics: """
-            for x in clusters:        
-                perf=getPerformanceAverage(svt.GetClusterMetric(x['name'],timerange=mrange,resolution=mresolution)['metrics'])
-                cn = (x['name'].split('.')[0]).replace('-','_')
+            for x in clusters:
+                perf = getPerformanceAverage(svt.GetClusterMetric(
+                        x['name'], timerange=mrange, resolution=mresolution)['metrics'])
+                cn = (x['name'].split('.')[0]).replace('-', '_')
                 for metricname in capacitymetric:
-                    scluster.labels(cn,metricname).set(x[metricname]/BtoGB)
+                    scluster.labels(cn, metricname).set(x[metricname]/BtoGB)
                 for metricname in dedupmetric:
-                    scluster.labels(cn,metricname).set(x[metricname].split()[0])
+                    scluster.labels(cn, metricname).set(x[metricname].split()[0])
                 for metricname in performancemetric:
-                    scluster.labels(cn,metricname).set(perf[metricname])
+                    scluster.labels(cn, metricname).set(perf[metricname])
                 for x in svt.GetClusterThroughput():
                     cn = x['source_omnistack_cluster_name']
-                    metricname=x['destination_omnistack_cluster_name']+' throughput'
-                    scluster.labels(cn,metricname).set(x['throughput'])
+                    metricname = x['destination_omnistack_cluster_name']+' throughput'
+                    scluster.labels(cn, metricname).set(x['throughput'])
 
             """  Node metrics: """
             for x in hosts:
-                y = getNodeCapacity(svt.GetHostCapacity(x['name'],timerange=mrange,resolution=mresolution)['metrics'])
-                perf=getPerformanceAverage(svt.GetHostMetrics(x['name'],timerange=mrange,resolution=mresolution)['metrics'])
-                cn = (x['name'].split('.')[0]).replace('-','_')
-                snode.labels(cn,'State').set(node_state[x['state']])
+                y = getNodeCapacity(svt.GetHostCapacity(x['name'], timerange=mrange, resolution=mresolution)['metrics'])
+                perf = getPerformanceAverage(svt.GetHostMetrics(
+                        x['name'], timerange=mrange, resolution=mresolution)['metrics'])
+                cn = (x['name'].split('.')[0]).replace('-', '_')
+                snode.labels(cn, 'State').set(node_state[x['state']])
                 for metricname in capacitymetric:
-                    snode.labels(cn,metricname).set(y[metricname])
+                    snode.labels(cn, metricname).set(y[metricname])
                 for metricname in dedupmetric:
-                    snode.labels(cn,metricname).set(y[metricname])
+                    snode.labels(cn, metricname).set(y[metricname])
                 for metricname in performancemetric:
-                    snode.labels(cn,metricname).set(perf[metricname])
-                        
+                    snode.labels(cn, metricname).set(perf[metricname])
+
             """  VM metrics: """
             for x in vms:
-                cn = (x['name'].split('.')[0]).replace('-','_')
-                svm.labels(cn,'state').set(vm_state[x['state']])                            
-                perf=getPerformanceAverage(svt.GetVMMetric(x['name'],timerange=mrange,resolution=mresolution)['metrics'])
+                cn = (x['name'].split('.')[0]).replace('-', '_')
+                svm.labels(cn, 'state').set(vm_state[x['state']])
+                perf = getPerformanceAverage(svt.GetVMMetric(
+                        x['name'], timerange=mrange, resolution=mresolution)['metrics'])
                 for metricname in performancemetric:
-                    svm.labels(cn,metricname).set(perf[metricname])                            
+                    svm.labels(cn, metricname).set(perf[metricname])
 
             """ DataStore metrics """
             for x in datastores:
-                cn = (x['name']).replace('-','_')
-                sdatastore.labels(cn,'size').set(x['size']/BtoGB)
-                        
+                cn = (x['name']).replace('-', '_')
+                sdatastore.labels(cn, 'size').set(x['size']/BtoGB)
+
             t1 = time.time()
             delta.set((t1-t0))
             while ((t1-t0) < mintervall):
                 time.sleep(1.0)
-                t1 = time.time()  
+                t1 = time.time()
         except KeyError:
-            log=logopen(path+lfile)
-            logwriter(log,"KeyError")
-            logwriter(log,str(e.expression))
-            logwriter(log,str(e.status))
-            logwriter(log,str(e.message))
-            logclose(log)                         
+            log = logopen(path+lfile)
+            logwriter(log, "KeyError")
+            logwriter(log, str(e.expression))
+            logwriter(log, str(e.status))
+            logwriter(log, str(e.message))
+            logclose(log)
         except SvtError as e:
             if e.status == 401:
                 try:
-                    log=logopen(path+lfile)
-                    logwriter(log,"Open Connection to SimpliVity")
-                    svt.Connect(svtuser,svtpassword)
-                    logwriter(log,"Connection to SimpliVity is open")
+                    log = logopen(path+lfile)
+                    logwriter(log, "Open Connection to SimpliVity")
+                    svt.Connect(svtuser, svtpassword)
+                    logwriter(log, "Connection to SimpliVity is open")
                     logclose(log)
                 except SvtError as e:
-                    log=logopen(path+lfile)
-                    logwriter(log,"Failed to open a conection to SimplVity")
-                    logwriter(log,str(e.expression))
-                    logwriter(log,str(e.status))
-                    logwriter(log,str(e.message))
-                    logwriter(log,"close SimpliVity connection")
+                    log = logopen(path+lfile)
+                    logwriter(log, "Failed to open a conection to SimplVity")
+                    logwriter(log, str(e.expression))
+                    logwriter(log, str(e.status))
+                    logwriter(log, str(e.message))
+                    logwriter(log, "close SimpliVity connection")
                     logclose(log)
                     exit(-200)
             elif e.status == 555:
-                log=logopen(path+lfile)
-                logwriter(log,'SvtError:')
-                logwriter(log,str(e.expression))
-                logwriter(log,str(e.status))
-                logwriter(log,str(e.message))
-                logclose(log)                            
-            else:
-                log=logopen(path+lfile)
-                logwriter(log,'Unhandeled SvtError:')
-                logwriter(log,str(e.expression))
-                logwriter(log,str(e.status))
-                logwriter(log,str(e.message))
-                logwriter(log,"close SimpliVity connection")
+                log = logopen(path+lfile)
+                logwriter(log, 'SvtError:')
+                logwriter(log, str(e.expression))
+                logwriter(log, str(e.status))
+                logwriter(log, str(e.message))
                 logclose(log)
-                exit(-200)        
+            else:
+                log = logopen(path+lfile)
+                logwriter(log, 'Unhandeled SvtError:')
+                logwriter(log, str(e.expression))
+                logwriter(log, str(e.status))
+                logwriter(log, str(e.message))
+                logwriter(log, "close SimpliVity connection")
+                logclose(log)
+                exit(-200)
